f98542779a23ed0523f7c8eb434f3c574f0afac7
unknown <jacobhaustveit@gmail.com>
2021-10-25 16:44:16 -0400

    Created Employee model, url, view, and html files.

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name= models.CharField( max_length=50)
+    address= models.CharField( max_length=200)
+    zipcode= models.IntegerField(max_length=10)
+    age= models.DateField(null=True, blank=True)
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.apps import apps
+from datetime import date
+
+from trash_collector.employees.models import Employee
 
 # Create your views here.
 
@@ -11,3 +15,16 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+
+
+@login_required
+def create(request):
+    logged_in_user= request.user
+    if request.method== "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zipcode_from_form= request.POST.get('zip_code')
+        age_from_form = request.POST.get('age')
+        new_empyloyee= Employee( name=name_from_form, address=address_from_form, zicode=zipcode_from_form, age=age_from_form)
+        new_empyloyee.save()
