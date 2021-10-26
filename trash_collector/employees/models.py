b4a48750401cd38b7208622cae8783a232f290f9
Ebony Riddick <ebonyriddick@gmail.com>
2021-10-26 17:17:23 -0400

    added ability for employee to edit profile: name, address, and zip code

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Employee(models.Model):
     name= models.CharField( max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
-    address= models.CharField( max_length=200)
+    address= models.CharField( max_length=200, null = True)
     zip_code= models.IntegerField(default=0)
     
     
